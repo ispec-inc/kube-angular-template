fe9b84454c4397d336b3285657ee6ee7bdc25a5e
Yusuke Yamada <yuhsukeshootsfc@gmail.com>
2020-01-21 11:16:45 +0900

    [add] codes.

--- install.sh
+++ install.sh
@@ -1,27 +1,33 @@
 git clone git@github.com:ispec-inc/kube-angular-template.git
-echo -n "application name?"
-read app_name
-echo -n "use dev? [y/n]"
-read use_dev
-echo -n "use stg? [y/n]"
-read use_stg
-echo -n "use github action? [y/n]"
+read -p "application name?: " app_name
+read -p "use dev? (y/N): " yn
+case "$yn" in
+    [yY]*) use_dev=true ;;
+    *) use_dev=false ;;
+esac
+read -p "use stg? (y/N): " yn
+case "$yn" in
+    [yY]*) use_dev=true ;;
+    *) use_dev=false ;;
+esac
 
 declare -a envs=()
 declare -a envs=("prod")
-if use_dev -eq 'y';then
+if $use_dev;then
 envs=('dev' "${envs[@]}")
 fi
 
-if use_stg -eq 'y';then
+if $use_stg;then
 envs=('stg' "${envs[@]}")
 fi
 
 mkdir k8s
-sed -i "s/{{app_name}}/$app_name/" manifest/**
+sed -i "s/{{app_name}}/$app_name/" manifest/deployment.yml
+sed -i "s/{{app_name}}/$app_name/" manifest/service.yml
+sed -i "s/{{app_name}}/$app_name/" manifest/config.yml
 for env in envs
 do
-cp kube-angular-template/manifest/depolyment.yml "${env}".depolyment.yml
-cp kube-angular-template/manifest/service.yml "${env}".service.yml
-cp kube-angular-template/manifest/config.yml "${env}".config.yml
+cp kube-angular-template/manifest/depolyment.yml k8s/"${env}".depolyment.yml
+cp kube-angular-template/manifest/service.yml k8s/"${env}".service.yml
+cp kube-angular-template/manifest/config.yml k8s/"${env}".config.yml
 done
